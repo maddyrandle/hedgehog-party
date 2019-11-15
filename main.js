8ca169cb7bea1ca416c75286db9d2d0ea69f756a
Madison Randle <madisonrandle25@gmail.com>
2019-11-14 18:41:17 -0700

    Dry code up

--- main.js
+++ main.js
@@ -1,39 +1,25 @@
-var hogletName = document.getElementById("name");
-var hoglet = document.getElementById("hoglets");
-var allergies = document.getElementById("allergies");
-var rsvpInfo = document.querySelector(".rsvp-info");
-var nameInput = document.querySelector(".name-input");
-var hogletInput = document.querySelector(".hoglet-input");
-var allergiesInput = document.querySelector(".allergies-input");
 var inviteBtn = document.querySelector(".invite-btn");
-var errorMessage = document.querySelector(".error-message")
-var form = document.querySelector(".input-form");
+var inviteListContainer = document.querySelector(".invite-list-container");
 
+inviteBtn.addEventListener("click", addRsvpInfo);
+inviteListContainer.addEventListener("click", removeRsvpInfo);
 
-inviteBtn.addEventListener("click", validateInput);
-
-//  I need help with this function
-function validateInput() {
-  if (name.value === "" || hoglet.value === "" || allergies.value === "") {
-   inviteBtn.disabled = true;
- } else if (name.value !== "" || hoglet.value !== "" || allergies.value !== "") {
-    inviteBtn.disabled = false;
-    showRsvpInfo();
-    console.log("invisible");
-  }
-}
-
-function showRsvpInfo() {
-  interpolateInfo();
-  rsvpInfo.classList.remove("hidden");
-  nameInput.classList.remove("hidden");
-  hogletInput.classList.remove("hidden");
-  allergiesInput.classList.remove("hidden");
-}
-
-function interpolateInfo() {
-  nameInput.insertAdjacentHTML("afterbegin", `${hogletName.value}`);
-  hogletInput.insertAdjacentHTML("afterbegin", `${hoglet.value}`);
-  allergiesInput.insertAdjacentHTML("afterbegin", `${allergies.value}`);
+function addRsvpInfo() {
+  var hogletName = document.querySelector(".name");
+  var numberOfHoglets = document.querySelector(".hoglets");
+  var allergies = document.querySelector(".allergies");
+  var form = document.querySelector(".rsvp-form");
+  inviteListContainer.innerHTML += `
+  <div class="rsvp-categories">
+    <p>${hogletName.value}</p>
+    <p>${numberOfHoglets.value}</p>
+    <p>${allergies.value}</p>
+    <button class="uninvite-btn" type="button">UNINVITE</button>
+  </div>
+  `
   form.reset();
+}
+
+function removeRsvpInfo(event) {
+  event.target.closest(".rsvp-categories").remove();
 }
